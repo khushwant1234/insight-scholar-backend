bf11a4f9f1126677a2fb1534506045b95d6c0a0b
Khushwant <khushwantsingh638@gmail.com>
2025-03-23 19:42:46 +0530

    changed cors origin

--- index.js
+++ index.js
@@ -23,8 +23,7 @@ import { User } from './Models/userModel.js';
 const app = express();
 const PORT = process.env.PORT || 8000;
 const corsOptions = {
-  origin: "https://insight-scholar.vercel.app" || process.env.FRONTEND_URL,
-  
+  origin: "*",
   credentials: true,
   methods: [ 'GET', 'POST', 'PUT', 'DELETE', 'OPTIONS' ],
   allowedHeaders: [ 'Content-Type', 'Authorization' ]
@@ -32,7 +31,7 @@ const corsOptions = {
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
   cors: {
-    origin: process.env.FRONTEND_URL || "https://insight-scholar.vercel.app",
+    origin: "*",
     methods: ["GET", "POST"]
   }
 });
@@ -123,5 +122,11 @@ app.use("/api/upvote", upvoteRouter);
 app.get('/', (req, res) => {
   res.send('API is running...');
 });
+
+app.use((req, res, next) => {
+  console.log(`Incoming request: ${req.method} ${req.url}`);
+  next();
+});
+
 
 // app.listen(PORT, () => console.log("Server Started on PORT: " + PORT));
